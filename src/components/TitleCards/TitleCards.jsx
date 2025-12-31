707dfc4861c465890dbb2813683c7b89fc94b21c
Dipika <maharjandipika49@gmail.com>
2025-12-31 11:00:00 +0545

    Add home page with Popular Now section

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './TitleCards.css';
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
-  return (
-    <div className='titlecards'>
-      
-    </div>
-  )
-}
+//   const cardsRef = useRef();
+//   const handleWheel = (event) => {
+//     event.preventDefault();
+//     cardsRef.current.scrollLeft += event.deltaY;
+//   };
 
-export default TitleCards
+//   useEffect(() => {
+//     cardsRef.current.addEventListener("wheel", handleWheel);
+//   }, []);
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" >
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="Netflix card" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
